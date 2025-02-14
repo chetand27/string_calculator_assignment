c3ff2f321080990342cc7b0ed86666abe3455301
chetan dhatrak <ketandhatrak@gmail.com>
2025-02-14 14:30:18 +0530

    modified add method to take single or multiple numbers and also handled test cases for both

--- test_string_calculator.rb
+++ test_string_calculator.rb
@@ -5,4 +5,12 @@ class StringCalculatorTest < Minitest::Test
   def test_empty_string
     assert_equal 0, StringCalculator.add('')
   end
+
+  def test_single_number
+    assert_equal 1, StringCalculator.add('1')
+  end
+
+  def test_two_number
+    assert_equal 6, StringCalculator.add('1,5')
+  end
 end
