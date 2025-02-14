447e64bc0998df4787736e1e765aac75422d15d0
chetan dhatrak <ketandhatrak@gmail.com>
2025-02-14 14:45:26 +0530

    handled newlines delimiters test case and the logic

--- test_string_calculator.rb
+++ test_string_calculator.rb
@@ -17,4 +17,8 @@ class StringCalculatorTest < Minitest::Test
   def test_multiple_numbers
     assert_equal 13, StringCalculator.add('1,5,7')
   end
+
+  def test_newline_delimiters
+    assert_equal 6, StringCalculator.add('1\n2,3')
+  end
 end
